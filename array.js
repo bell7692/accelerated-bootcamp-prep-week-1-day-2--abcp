d9c0ab443610ed776a7fa922eb136a5d0a2ff5ca
Bo Eun Lee <bolee417@gmail.com>
2018-01-14 17:40:37 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,17 +1,21 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+var newArray = [element, ...array]
+  
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+array.unshift(element);
+  
 }
 
 function addElementToEndOfArray(array, element) {
-
+var newArray2 = [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+array.push(element);
 }
 
 function accessElementInArray(array, index) {
